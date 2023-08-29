086d6e8e7ba7aa23abe0a9ac3686a7ffd7ff5d20
Lasse <lasseronnlie@hotmail.com>
2023-08-29 16:07:48 +0200

    github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  base: './dist',
+  base: '/AuctionHouse/',
   plugin: [
     // default settings on build (i.e. fail on error)
     {
