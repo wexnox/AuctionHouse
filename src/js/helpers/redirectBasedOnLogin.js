fe2c99fbfd8b2440070b300ed560957b5fbca794
Lasse R <lasseronnlie@hotmail.com>
2024-03-04 08:19:04 +0100

    Refactor redirectBasedOnLogin function for clarity

---
+++ src/js/helpers/redirectBasedOnLogin.js
@@ -0,0 +1,14 @@
+export function redirectBasedOnLogin(pathname) {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+        redirectToURL(pathname, ["/auth/profile.html", "/auth/register.html"], "/profile");
+    } else {
+        redirectToURL(pathname, ["/profile/", "/profile/details.html"], "/auth/profile.html");
+    }
+}
+
+function redirectToURL(pathname, paths, redirectTo) {
+    if (paths.includes(pathname)) {
+        location.href = redirectTo;
+    }
+}
