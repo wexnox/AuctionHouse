a62190b202abd98aba02cff8e49e6883efbb25f1
Lasse R <lasseronnlie@hotmail.com>
2024-03-04 08:19:19 +0100

    Remove unused Bootstrap imports in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
 import '../scss/index.scss';
 import router from './router.js';
-
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+// TODO: Remove comments
 
 router();
-
-
-
