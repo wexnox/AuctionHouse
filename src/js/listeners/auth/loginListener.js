4e4b5838c25578f143906f613893952a46de9516
Lasse R <lasseronnlie@hotmail.com>
2024-03-04 08:25:24 +0100

    Refactor login handling and update import path

--- src/js/listeners/auth/loginListener.js
+++ src/js/listeners/auth/loginListener.js
@@ -1,23 +1,22 @@
-import {login} from "../../api/auth/index.js";
+import {login} from "../../api/auth/login.js";
+
 
 export function loginListener() {
+
+
     const form = document.getElementById("loginForm");
 
     if (form) {
-        form.addEventListener("submit", handleLogin);
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const formData = new FormData(event.target);
+            const userProfile = Object.fromEntries(formData.entries());
+
+            console.log(userProfile); // Add this line to debug
+            console.log(JSON.stringify(userProfile));
+
+            login(userProfile);
+        });
     }
-}
-
-async function handleLogin(event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const userProfile = Object.fromEntries(formData.entries());
-
-
-    const button = form.querySelector("button");
-    button.innerText = "Logging in...";
-
-    login(userProfile);
-
 }
