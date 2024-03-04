cb0cdac8f9f41fe67757ef3024666a6d84ea8c75
Lasse R <lasseronnlie@hotmail.com>
2024-03-04 08:27:13 +0100

    Implement new function for updating user avatars.

---
+++ src/js/api/profile/updateAvatar.js
@@ -0,0 +1,35 @@
+import {API_MAIN_URL} from "../constants.js";
+import {authFetch} from "../api.js";
+import {getTokenFromStorage} from "../../helpers/storage.js";
+
+const action = "/profiles";
+const method = "PUT";
+
+export async function updateUserAvatar(userData) {
+    const user = getTokenFromStorage("profile");
+    const userName = user.name;
+    const updateAvatarURL = `${API_MAIN_URL}${action}/${userName}/media`;
+
+    console.log(`Attempting to update avatar for user: ${userName}`);
+
+    if (!user) {
+        throw new Error("This requires a valid user!");
+    }
+
+    const response = await authFetch(updateAvatarURL, {
+        method,
+        body: JSON.stringify({avatar: userData.avatar}),
+    });
+    console.log("Server response: ", response);
+
+    if (response.ok) {
+        let responseJson = await response.json();
+        console.log("Server response JSON: ", responseJson);
+        return responseJson;
+    } else {
+        // If the server response is not OK, then throw an error
+        console.error(`Server responded with status: ${response.status}`);
+        // If the server response is not OK, then throw an error
+        throw new Error(`Server responded with status: ${response.status}`);
+    }
+}
