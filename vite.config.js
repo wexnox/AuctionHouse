c751fd20171dda1f0dba07f9d3d61a7508b46c3d
Lasse R <lasseronnlie@hotmail.com>
2024-03-04 08:16:42 +0100

    Refactor vite configuration and adjust project structure

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,30 @@
-import eslint from 'vite-plugin-eslint';
 import {resolve} from 'path';
 import {defineConfig} from 'vite';
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-    root: resolve(__dirname, './src'),
+    root: resolve(__dirname, 'src'),
     base: '/',
 
-    resolve: {
-        alias: {
-            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
-        },
-    },
-
-    plugin: [
-        {
-            ...eslint(),
-            apply: 'build',
-        }, {
-            ...eslint({
-                failOnWarning: false,
-                failOnError: false,
-            }),
-            apply: 'serve',
-            enforce: 'post',
-        },
+    plugins: [
+        eslintPlugin()
     ],
 
     build: {
         rollupOptions: {
             input: {
                 index: resolve(__dirname, './src/index.html'),
-                login: resolve(__dirname, './src/auth/login/login.html'),
-                register: resolve(__dirname, './src/auth/register/index.html'),
+                login: resolve(__dirname, './src/auth/login.html'),
+                register: resolve(__dirname, './src/auth/register.html'),
                 profile: resolve(__dirname, './src/profile/index.html'),
+                createListing: resolve(__dirname, './src/listings/create.html'),
+                listingsDetails: resolve(__dirname, './src/listings/details.html'),
+                search: resolve(__dirname, './src/search.html')
+
             },
         },
 
         emptyOutDir: true,
-        outDir: resolve(__dirname, './docs'),
+        outDir: resolve(__dirname, '../docs'),
     },
 });
