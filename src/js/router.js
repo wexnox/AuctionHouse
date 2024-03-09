27bc2765093bee0b2a596922683def17e3c5d60c
Lasse R <lasseronnlie@hotmail.com>
2024-03-09 03:56:15 +0100

    Refactored router.js and reorganized listeners for usability.

--- src/js/router.js
+++ src/js/router.js
@@ -1,9 +1,10 @@
 import * as listeners from './listeners/index.js';
 import buildMenu from './ui/common/buildMenu.js';
-import {redirectBasedOnLogin} from './helpers/auth/index.js';
+import {redirectBasedOnLogin} from './helpers/redirectBasedOnLogin.js';
 
 export default function router() {
     const pathname = location.pathname;
+    let listAuctions;
 
     redirectBasedOnLogin(pathname);
     buildMenu(pathname);
@@ -11,17 +12,22 @@ export default function router() {
     switch (pathname) {
         case "/":
         case "/index.html":
-            listeners.getAllListingsListener()
-            break;
-        case "/auth/login.html":
-            listeners.loginListener();
+            listAuctions = listeners.getAllAuctionsListener()
+            listAuctions() // Initial auctions load
+            listeners.loadMore(listAuctions) // Set up listener for subsequent loads
             break;
         case "/auth/register.html":
             listeners.setRegisterUserListener();
             break;
+        case "/auth/login.html":
+            listeners.loginListener();
+            break;
         case "/profile/index.html":
-            listeners.fetchProfile()
+            listeners.userProfileListener()
+            listeners.updateAvatarListener();
             return;
+        case "/listings/create.html":
+            listeners.createNewListingListener()
     }
 
 }
