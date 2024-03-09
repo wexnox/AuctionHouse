c3c6d868c5f897c5f514b90357c0200378825d5d
Lasse R <lasseronnlie@hotmail.com>
2024-03-09 04:00:28 +0100

    Relocated constants.js and added new constants

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,15 @@
 export const API_HOST_URL = 'https://api.noroff.dev/api/v1';
+
 export const API_AUCTION = '/auction';
+
 export const API_MAIN_URL = `${API_HOST_URL}${API_AUCTION}`;
 
 export const API_PROFILE_URL = `${API_MAIN_URL}/profiles`;
 
+export const listingsURL = "/listings";
+
+export const bidsURL = "/bids";
+
+export const sellerBidsURL = "?_seller=true&_bids=true";
+
+export const AUCTIONS_LIMIT = 10;
