15fa8597d11edc6dcdac738673d3b59cdabd5560
Lasse R <lasseronnlie@hotmail.com>
2025-05-10 02:33:46 +0200

    Add Search to auth

--- src/js/routes/authRoutes.js
+++ src/js/routes/authRoutes.js
@@ -1,6 +1,7 @@
 // Handles all routes related to authentication (login, register)
 import { setPageTitle } from '../utils/titleManager.js';
 import * as listeners from '../listeners/index.js';
+import { initializeSearch } from '@/js/ui/search.js';
 
 export function handleAuthRoutes(pathname) {
 
@@ -8,11 +9,15 @@ export function handleAuthRoutes(pathname) {
 
     setPageTitle('Register');
 
+    initializeSearch(posts);
+
     listeners.setRegisterUserListener();
 
   } else if (pathname.endsWith('/login.html')) {
 
     setPageTitle('Login');
+
+    initializeSearch(posts);
 
     listeners.loginListener();
   }
