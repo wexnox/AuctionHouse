a7a7d59987283fbb653524413e39e61f880afa52
Lasse R <lasseronnlie@hotmail.com>
2025-08-20 18:50:27 +0200

    Refactor riktig håndtering av FormData

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -1,8 +1,9 @@
 import {headers} from '@/js/api/headers.js';
 
 export async function authFetch(url, options = {}) {
+  const isFormData = options && options.body && typeof FormData !== 'undefined' && options.body instanceof FormData;
   return fetch(url, {
     ...options,
-    headers: headers(Boolean(options.body)),
+    headers: headers(isFormData ? false : Boolean(options.body)),
   });
 }
